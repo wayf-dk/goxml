1253912e995a05245b447a20a77690c0b1fedc6a
Mehran Khan <mekhan@test-wayf-03.test.lan>
2018-08-10 09:19:27 +0000

    Added Comments to Code

--- goxml.go
+++ goxml.go
@@ -320,6 +320,9 @@ func NewHtmlXp(html []byte) (xp *Xp) {
 	return
 }
 
+/*
+  DocGetRootElement returns the root element of the document
+*/
 func (xp *Xp) DocGetRootElement() types.Node {
 	libxml2Lock.Lock()
 	defer libxml2Lock.Unlock()
@@ -327,6 +330,9 @@ func (xp *Xp) DocGetRootElement() types.Node {
 	return root
 }
 
+/*
+  RM removes the node
+*/
 func (xp *Xp) Rm(context types.Node, path string) {
 //	libxml2Lock.Lock()
 //	defer libxml2Lock.Unlock()
@@ -342,6 +348,9 @@ func (xp *Xp) Rm(context types.Node, path string) {
 	}
 }
 
+/*
+  RmElement removes an element in a Node
+*/
 func RmElement(element types.Node) {
 	libxml2Lock.Lock()
 	defer libxml2Lock.Unlock()
@@ -350,6 +359,9 @@ func RmElement(element types.Node) {
     element.Free()
 }
 
+/*
+  freeElement refers to freeing the memory
+*/
 func freeElement(element types.Node) {
 	libxml2Lock.Lock()
 	defer libxml2Lock.Unlock()
@@ -386,6 +398,9 @@ func (xp *Xp) C14n(node types.Node, nsPrefixes string) (s string) {
 	return
 }
 
+/*
+  Dump dumps the whole document
+*/
 func (xp *Xp) Dump() []byte {
 	libxml2Lock.Lock()
 	defer libxml2Lock.Unlock()
