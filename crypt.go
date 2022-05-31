9106a267f6f16e1314cbaa7414193a76939729af
Mads Freek <freek@wayf.dk>
2022-05-31 18:33:23 +0200

    Fixed name of aes192-cbc - everywhere ...

--- crypt.go
+++ crypt.go
@@ -305,7 +305,7 @@ func (xp *Xp) Decrypt(encryptedAssertion types.Node, privatekey, pw []byte) (err
 	}
 
 	switch encryptionMethod {
-	case "http://www.w3.org/2001/04/xmlenc#aes128-cbc", "http://www.w3.org/2009/xmlenc11#aes192-cbc", "http://www.w3.org/2001/04/xmlenc#aes256-cbc":
+	case "http://www.w3.org/2001/04/xmlenc#aes128-cbc", "http://www.w3.org/2001/04/xmlenc#aes192-cbc", "http://www.w3.org/2001/04/xmlenc#aes256-cbc":
 		decrypt = decryptCBC
 	case "http://www.w3.org/2009/xmlenc11#aes128-gcm", "http://www.w3.org/2009/xmlenc11#aes192-gcm", "http://www.w3.org/2009/xmlenc11#aes256-gcm":
 		decrypt = decryptGCM
